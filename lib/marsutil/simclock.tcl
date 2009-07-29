21a2a79e41edcea5f5bc9e565f74662e24aea92d
Will Duquette <William.H.Duquette@jpl.nasa.gov>
2009-07-29 22:30:32 +0000

    Fixed Bug #{2216}
    Added the required commands to simclock(n) and updated the 
    test suite and man page.

--- lib/marsutil/simclock.tcl
+++ lib/marsutil/simclock.tcl
@@ -832,6 +832,143 @@ snit::type ::marsutil::simclockType {
         expr {int(round(double($minutes)*$factor(minutes,$units)/$tickSize))}
     }
 
+    # fromTimeSpec spec
+    #
+    # spec           A time-spec string
+    #
+    # Converts a time-spec string to a sim time in ticks.
+    #
+    # A time-spec string specifies a time in ticks as a base time 
+    # optionally plus or minus an offset.  The offset is always in ticks;
+    # the base time can be a time in ticks, a zulu time-string, or
+    # a "named" time, e.g., "T0" or "NOW".  If the base time is omitted, 
+    # "NOW" is assumed.  For example,
+    #
+    #    +5             simclock now 5
+    #    -5             simclock now -5
+    #    <zulu>+30      Zulu-time plus 30 ticks
+    #    NOW-30         simclock now -30
+    #    40             40
+    #    40+5           45
+    #    T0+45          45
+    #    T0             0
+
+    method fromTimeSpec {spec} {
+        # FIRST, split the spec into base time, op, and offset.
+        set result [regexp -expanded {
+            # FIRST, Start from beginning of string
+            ^
+
+            # NEXT, capture the base time, which (at this point)
+            # can be any string that doesn't contain +, -, or whitespace.
+            # It can, however, be empty.
+            ([^-+[:space:]]*)
+
+            # NEXT, skip any amount of white space
+            \s*
+
+            # NEXT, capture the offset, if any
+            (
+
+            # NEXT, it begins with a + or -, which we need to capture
+            ([-+])
+
+            # NEXT, skip any amount of white space
+            \s*
+ 
+            # NEXT, the actual offset is an arbitrary integer at least
+            # one character long
+            (\d+)
+
+            # NEXT, we need 0 or 1 offsets, including the operator and
+            # the number.
+            )?
+
+            # NEXT, continue to the end of the string.
+            $
+        } $spec dummy basetime dummy2 op offset]
+
+        if {!$result} {
+            error "invalid time spec \"$spec\", should be <basetime><+/-><offset>"
+        }
+
+        # NEXT, convert the base time to ticks
+        set basetime [string toupper $basetime]
+
+        if {$basetime eq "T0"} {
+            set t 0
+        } elseif {$basetime eq "NOW" || $basetime eq ""} {
+            set t [$self now]
+        } elseif {[string is integer -strict $basetime]} {
+            set t $basetime
+        } elseif {![catch {$self fromZulu $basetime} result]} {
+            set t $result
+        } else {
+            error "invalid time spec \"$spec\", base time should be \"NOW\", \"T0\", an integer tick, or a zulu-time string"
+        }
+
+        if {$offset ne ""} {
+            incr t $op$offset
+        }
+
+        return $t
+    }
+
+    #-------------------------------------------------------------------
+    # Validation Routines
+
+    # timespec validate spec
+    #
+    # spec     Possibly, a time spec
+    #
+    # Validates it as a time spec, throwing INVALID if invalid, and
+    # returning the time in ticks if valid.
+
+    method {timespec validate} {spec} {
+        if {[catch {$self fromTimeSpec $spec} result]} {
+            return -code error -errorcode INVALID $result
+        } else {
+            return $result
+        }
+    }
+
+
+    # past validate spec
+    #
+    # spec     Possibly, a time spec
+    #
+    # Validates it as a time spec for a time no later than "now", 
+    # throwing INVALID if invalid, and returning the time in ticks if valid.
+
+    method {past validate} {spec} {
+        if {[catch {$self fromTimeSpec $spec} result]} {
+            return -code error -errorcode INVALID $result
+        } elseif {$result < 0 || $result > [$self now]} {
+            return -code error -errorcode INVALID \
+                "invalid time spec \"$spec\", expected time between T0 and NOW"
+        } else {
+            return $result
+        }
+    }
+
+    # future validate spec
+    #
+    # spec     Possibly, a time spec
+    #
+    # Validates it as a time spec for a time no earlier than "now", 
+    # throwing INVALID if invalid, and returning the time in ticks if valid.
+
+    method {future validate} {spec} {
+        if {[catch {$self fromTimeSpec $spec} result]} {
+            return -code error -errorcode INVALID $result
+        } elseif {$result < [$self now]} {
+            return -code error -errorcode INVALID \
+              "invalid time spec \"$spec\", expected time no earlier than NOW"
+        } else {
+            return $result
+        }
+    }
+
     #-------------------------------------------------------------------
     # Utility Methods and Procs
 
