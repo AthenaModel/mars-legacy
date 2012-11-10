6275a9ad0e6bfa7d2323e8c818ec507c86e72e15
Katie Altimus <Katie.Altimus@jpl.nasa.gov>
2012-11-10 03:34:10 +0000

    I added a print icon to the report viewer, as well as a print method which will print the active report. If no active report is displayed, and the user prints, it displays a popup. Additionally there are error popups if the temp file cannot be created and also if printing fails.

--- lib/marsgui/reportviewer.tcl
+++ lib/marsgui/reportviewer.tcl
@@ -60,13 +60,21 @@ snit::widget ::marsgui::reportviewer {
 
     component reptext            ;# Text widget
     component hotbtn             ;# "Hot List" checkbutton
+    component print              ;# The print button
 
     #-------------------------------------------------------------------
     # Instance Variables
 
-    variable currentReport -1     ;# ID of currently displayed report
-    variable hot           0      ;# Whether the report is on the hot list
-                                   # or not.
+    variable currentReport -1       ;# ID of currently displayed report
+    variable hot           0        ;# Whether the report is on the hot list
+                                     # or not.
+    variable ignoreNoCurrentReport 0;# Whether or not to display the popup
+                                     # if a user tries to print when no
+                                     # current report is displayed.
+    variable ignoreNoFileSpace     0;# Whether or not to display the popup
+                                     # if a file cannot be created.
+    variable ignorePrintError     0; # Whether or not to display the popup
+                                     # if there is a print error
 
     #-------------------------------------------------------------------
     # Constructor
@@ -87,7 +95,16 @@ snit::widget ::marsgui::reportviewer {
             -state    disabled                            \
             -command  [mymethod Toggle]
 
+        install print using ttk::button $bar.print        \
+            -image       ::marsgui::icon::print           \
+            -state       disabled                            \
+            -command     [mymethod print]
+
+        DynamicHelp::add $bar.print \
+            -text "Print the displayed active report"     
+
         pack $bar.label  -side left
+        pack $bar.print  -side right
         pack $bar.hotbtn -side right
 
         # NEXT, create the rotext widget
@@ -133,7 +150,69 @@ snit::widget ::marsgui::reportviewer {
     method Toggle {} {
         reporter hotlist set $currentReport $hot
     }
-    
+
+    # print
+    #
+    # Prints the actively displayed report.
+
+    method print {} {
+        # FIRST, if currentReport = -1, there is no actively displayed report.
+        #        The only manner in which this print can be called in that 
+        #        case is through the File->Print menu, so display a popup that
+        #        states there is no active report selected to print.
+
+        if {$currentReport == -1} {
+            # NEXT, there is no active report selected. Display the pop up error
+            messagebox popup                                     \
+                -buttons   {ok "OK"}                             \
+                -title     "Error Printing: No Report Selected!" \
+                -parent    $win                                  \
+                -message   "There is no active report selected to print! \nPlease select a report to print before using the following print methods:\n\n      File -> Print\n      <Ctrl-p>\n      <Alt-f Alt-p>"
+#                -ignoretag $ignoreNoCurrentReport                \
+        } else {
+            # NEXT, there *IS* an active report selected. Try to print.
+            set filename "report.txt"
+            if {[catch {
+                # NEXT, open the filename for writing
+                set fileId [open $filename "w"]
+                # NEXT, send the data to the file 
+                puts $fileId [$reptext get 1.0 end]
+                # NEXT, close the file, ensuring the data is written
+                # before you continue printing.
+                close $fileId
+            } result opts]} {
+                # NEXT, there was an error creating the temporary file. 
+                #       Make a popup.
+                messagebox popup                                      \
+                    -buttons   {ok "OK"}                              \
+                    -title     "Error Printing: Error Creating File!" \
+                    -parent    $win                                   \
+                    -icon      error                                  \
+                    -message   "There was an error creating the temporary file used for printing. Please check that you have available memory and user permissions to write files to the current working directory!"
+#                    -ignoretag $ignoreNoFileSpace                          \
+            } else {
+                # NEXT, The file was made correctly. Try to print the file. 
+                #       It goes to the default printer.
+                if {[catch {
+                    exec cat $filename | lpr
+                    # Delete the file used, to keep from cluttering filesystem.
+                    exec rm $filename
+                } result opts]} {
+                    # NEXT, there was an error printing. Make a popup
+                    messagebox popup                   \
+                        -buttons   {ok "OK"}           \
+                        -title     "Error Printing!!!" \
+                        -parent    $win                \
+                        -icon      error               \
+                        -message   "There was an error printing the report! Please verify that you have a default printer setup on this machine!"
+#                        -ignoretag $ignorePrintError   \
+                } else {
+                    # NEXT, it printed ok. Continue.
+                } ;# End if/else for catching printing errors
+            } ;# End if/else for catching file creation errors.
+        } ;# End if/else for whether a currentReport is active or not.
+    }
+
     # Method: Log
     #
     # Logs a status message by calling the <-logcmd>.
@@ -181,7 +260,6 @@ snit::widget ::marsgui::reportviewer {
 
 
             $reptext ins end "Title:\t$row(title)\n"  $tags
-
             $reptext ins end "\n"
             $reptext ins end $row(text)
             $reptext see 1.0
@@ -189,6 +267,8 @@ snit::widget ::marsgui::reportviewer {
 
             set hot $row(hotlist)
             $hotbtn configure -state normal
+            $print  configure -state normal
+
         }
 
         set currentReport $id
@@ -206,6 +286,7 @@ snit::widget ::marsgui::reportviewer {
         set currentReport -1
         set hot 0
         $hotbtn configure -state disabled
+        $print  configure -state disabled
     }
 
 
